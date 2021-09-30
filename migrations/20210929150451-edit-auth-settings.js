ec182c14568a7751ea5fca68172963ab753e7c57
takasany <jackmacman2@gmail.com>
2021-09-30 20:29:59 +0900

    マイグレーションを実行してテーブルのカラムの設定を変更 #60

--- migrations/20210929150451-edit-auth-settings.js
+++ migrations/20210929150451-edit-auth-settings.js
@@ -4,7 +4,6 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     queryInterface.changeColumn("users", "password", {
       type: Sequelize.STRING,
-      unique: true,
       allowNull: false,
     });
     queryInterface.changeColumn("users", "username", {
