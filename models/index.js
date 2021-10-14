5737aa32728f34772a5a9fd876bdfb949d672123
takasany <jackmacman2@gmail.com>
2021-10-14 23:33:08 +0900

    Sequelizeが参照する環境変数の参照先を再度変更 #67

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = process.env.NODE_ENV
+const config = process.env.NODE_ENV === "production"
   ? process.env
   : require(__dirname + "/../config/config.json")[env];
 const db = {};
