355ef29e6fc3eb94913ced33602e6e2558916bc9
takasany <jackmacman2@gmail.com>
2021-11-10 11:39:31 +0900

    フォローモデルの設定を修正 #96

--- models/follow.js
+++ models/follow.js
@@ -1,36 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class follow extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
-  };
-  follow.init({
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+  }
+  follow.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      followId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
     },
-    user: {
-      type: DataTypes.JSON,
-      allowNull: false,
-    },
-    follow: {
-      type: DataTypes.JSON,
-      allowNull: false,
+    {
+      sequelize,
+      modelName: "follow",
     }
-  }, {
-    sequelize,
-    modelName: 'follow',
-  });
+  );
   return follow;
 };
